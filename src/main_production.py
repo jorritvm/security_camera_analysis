1a21bf0e09fd69df48ce5ffcf9bb48331937693d
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2025-09-03 09:17:13 +0200

    adding two entrypoints (one for bench, one for prod)

---
+++ src/main_production.py
@@ -0,0 +1,4 @@
+"""
+entrypoint module to run the code in production mode
+this will read from config.py
+"""
---
+++ src/main_yolo_benchmark.py
@@ -0,0 +1,4 @@
+"""
+entrypoint module to benchmark YOLO model performance so we can define the proper settings for production
+the resulting optimized constants will be set up in config.py
+"""
